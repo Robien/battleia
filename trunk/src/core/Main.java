815c7b82eafcfa2fcd3d2312210a87e0b038abe9
r.guyard <r.guyard@55b8c863-d013-4a29-c4b6-ca117c38479e>
2014-05-07 04:27:35 +0000

    try to fix

--- trunk/src/core/Main.java
+++ trunk/src/core/Main.java
@@ -1,5 +1,8 @@
 package core;
 
+import WoodooDragonBase.WoodooDragonBase;
+import romainAIv2.RomainAiv21;
+import segfault.SegFaultAI;
 import tools.Log;
 import core.moteur.IAManager;
 import core.moteur.IAManager.printStatsToFile;
@@ -18,6 +21,9 @@ public class Main
         // ajouter ici son IA
         iaManager.declareIA(new DumyAI());
         iaManager.declareIA(new DumyAIVariante());
+        iaManager.declareIA(new RomainAiv21());
+        iaManager.declareIA(new WoodooDragonBase());
+        iaManager.declareIA(new SegFaultAI());
         // iaManager.declareIA(new MonIAIci());
 
         // c'est parti !

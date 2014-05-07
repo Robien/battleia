e29bb6e5512c97bb8d10ccd7e84bbdeed7d7ce0a
r.guyard <r.guyard@55b8c863-d013-4a29-c4b6-ca117c38479e>
2014-05-07 04:33:58 +0000

    remise en etat du main ... desole sa va conflit ...

--- trunk/src/core/Main.java
+++ trunk/src/core/Main.java
@@ -1,8 +1,5 @@
 package core;
 
-import WoodooDragonBase.WoodooDragonBase;
-import romainAIv2.RomainAiv21;
-import segfault.SegFaultAI;
 import tools.Log;
 import core.moteur.IAManager;
 import core.moteur.IAManager.printStatsToFile;
@@ -21,9 +18,6 @@ public class Main
         // ajouter ici son IA
         iaManager.declareIA(new DumyAI());
         iaManager.declareIA(new DumyAIVariante());
-        iaManager.declareIA(new RomainAiv21());
-        iaManager.declareIA(new WoodooDragonBase());
-        iaManager.declareIA(new SegFaultAI());
         // iaManager.declareIA(new MonIAIci());
 
         // c'est parti !

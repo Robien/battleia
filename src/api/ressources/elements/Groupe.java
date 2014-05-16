48834bde878d719a030a4cf5dac0d629ea7e8523
r.guyard <r.guyard@55b8c863-d013-4a29-c4b6-ca117c38479e>
2014-05-16 07:53:15 +0000

    début de la gestion des groupes

--- src/api/ressources/elements/Groupe.java
+++ src/api/ressources/elements/Groupe.java
@@ -1,34 +1,53 @@
 package api.ressources.elements;
 
+import api.IA.InfosBase;
 import api.ressources.Environement;
 import core.ressources.Constantes.typeUnite;
 
 public class Groupe
 {
-    private int[]    units;
-    private Case[][] autour       = null;
-    private Case     destination  = null;
-    private int      tempsRestant = 0;
+    private int[]      units;
+    private Case[][]   autour       = null;
+    private Case       destination  = null;
+    private int        tempsRestant = 0;
+
+    private final int  id;
+    private static int nbGroupe     = 0;
 
     public Groupe()
     {
+        id = nbGroupe++;
         init();
     }
 
     public Groupe(Case[][] position)
     {
+        id = nbGroupe++;
         init();
         autour = position;
     }
 
     public Groupe(Groupe g)
     {
+        id = g.id;
         init();
         for (typeUnite u : typeUnite.values())
         {
             units[u.ordinal()] = g.getNbUnite(u);
         }
         autour = g.autour;
+        destination = g.destination;
+    }
+
+    public Groupe(Groupe g, InfosBase info)
+    {
+        id = g.id;
+        init();
+        for (typeUnite u : typeUnite.values())
+        {
+            units[u.ordinal()] = g.getNbUnite(u);
+        }
+        autour = info.caseAutourBase;
         destination = g.destination;
     }
 
@@ -94,6 +113,11 @@ public class Groupe
             tempsRestant = (int) (Environement.get().getDistance(autour[1][1], destination) / Environement.get().getVitesseGroupe(this));
             return tempsRestant;
         }
+    }
+
+    public int getId()
+    {
+        return id;
     }
 
 }
